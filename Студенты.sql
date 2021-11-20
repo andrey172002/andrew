dca8e7d8cd9e7d16dd2b23e8ab6efe65f1b7af4d
Andrey Odoev <60692782+andrey172002@users.noreply.github.com>
2021-11-20 13:14:39 +0300

    Add files via upload

--- Студенты.sql
+++ Студенты.sql
@@ -1,30 +1,16 @@
 --
--- File generated with SQLiteStudio v3.2.1 on Пт мар 19 12:31:36 2020
+-- Файл сгенерирован с помощью SQLiteStudio v3.3.3 в Сб ноя 20 13:12:11 2021
 --
--- Text encoding used: System
+-- Использованная кодировка текста: System
 --
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
--- Table: Предметы
-CREATE TABLE Предметы (
-    [Код предмета]          BIGINT       PRIMARY KEY
-                                         UNIQUE
-                                         NOT NULL,
-    [Наименование предмета] VARCHAR (50),
-    [Описание предмета]     TEXT
-);
+-- Таблица: Предметы
+CREATE TABLE Предметы ("Код предмета" BIGINT PRIMARY KEY UNIQUE NOT NULL, "Наименование предмета" VARCHAR (50), "Описание предмета " TEXT);
 
-
--- Table: Специальности
-CREATE TABLE Специальности (
-    [Код специальности]          BIGINT       PRIMARY KEY
-                                              NOT NULL
-                                              UNIQUE,
-    [Наименование специальности] VARCHAR (50),
-    [Описание специальности]     TEXT
-);
-
+-- Таблица: Специальности
+CREATE TABLE Специальности ("Код специальности " BIGINT PRIMARY KEY UNIQUE NOT NULL, "Наименование специальности " VARCHAR (50), "Описание специальности " TEXT);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
